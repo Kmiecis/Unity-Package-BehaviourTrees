58df0f618f0f7017af1088178b3b4dc26a3d46f2
Sławomir Kmiecik <slawekkmiecik@gmail.com>
2024-02-10 07:58:19 +0100

    Tidy up

--- Editor/Extensions/Extensions.cs
+++ Editor/Extensions/Extensions.cs
@@ -85,61 +85,6 @@ namespace CommonEditor.BehaviourTrees
             int index = self.arraySize - 1;
             self.DeleteArrayElementAtIndex(index);
         }
-
-        public static object GetValue(this SerializedProperty self)
-        {
-            var sanitizedPath = self.propertyPath.Replace(".Array.data[", "[");
-            object result = self.serializedObject.targetObject;
-
-            var elements = sanitizedPath.Split('.');
-            foreach (var element in elements)
-            {
-                if (element.Contains("["))
-                {
-                    var elementName = element.Substring(0, element.IndexOf("["));
-                    var strIndex = element.Substring(element.IndexOf("[")).Replace("[", string.Empty).Replace("]", string.Empty);
-                    var index = int.Parse(strIndex);
-                    result = GetValueFromList(result, elementName, index);
-                }
-                else
-                {
-                    result = GetValueFromType(result, element);
-                }
-            }
-            return result;
-        }
-
-        private static object GetValueFromList(object source, string name, int index)
-        {
-            var list = GetValueFromType(source, name) as System.Collections.IList;
-            if (list != null)
-            {
-                return list[index];
-            }
-            return null;
-        }
-
-        private static object GetValueFromType(object source, string name)
-        {
-            var type = source.GetType();
-            while (type != null)
-            {
-                var field = type.GetField(name, BindingFlags.NonPublic | BindingFlags.Public | BindingFlags.Instance);
-                if (field != null)
-                {
-                    return field.GetValue(source);
-                }
-
-                var property = type.GetProperty(name, BindingFlags.NonPublic | BindingFlags.Public | BindingFlags.Instance | BindingFlags.IgnoreCase);
-                if (property != null)
-                {
-                    return property.GetValue(source, null);
-                }
-
-                type = type.BaseType;
-            }
-            return null;
-        }
         #endregion
 
         #region String
